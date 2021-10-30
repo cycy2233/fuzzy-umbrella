be9a44e688e14a4fa16f00bea6631e7ce1c84686
cycy2233 <90803015+cycy2233@users.noreply.github.com>
2021-10-30 17:46:48 -0400

    project 03

---
+++ exercises/intro-to javascript/script.js
@@ -0,0 +1,19 @@
+// alert("Hello World")
+// console.log("Hello Wolrd")
+/*function sayHello(){
+    console.log("Hi")
+}
+sayHello()*/
+
+/*let age = 25;
+let height = 5;
+
+console.log(age);
+console.log(height);*/
+
+let noun = "apple";
+let adjactive = "sweet";
+let verb = "eat";
+
+let senctence = `I want to ${verb} a ${adjactive} ${noun}`
+console.log(senctence);
